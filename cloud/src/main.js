b39e6ba430de9c3c0295afe2a7f614a4e96ea62d
AlipourIm <iman.alip2001@gmail.com>
2022-08-18 21:45:50 +0430

    add corresponding cloud functions!

--- cloud/src/main.js
+++ cloud/src/main.js
@@ -4,3 +4,4 @@ import './cloudFunctions/get-whiteboard';
 import './cloudFunctions/remove-all-nodes';
 import './cloudFunctions/remove-node';
 import './cloudFunctions/change-team-state';
+import './cloudFunctions/mentors-in-room';
